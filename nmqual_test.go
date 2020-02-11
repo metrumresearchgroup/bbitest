c6506a4940d226cda36fb156010ca357e9aeee9d
Darrell Breeden <dukeofubuntu@gmail.com>
2020-02-11 14:00:11 -0500

    Updating to include test for NMQual

---
+++ nmqual_test.go
@@ -0,0 +1,53 @@
+package babylontest
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestNMQUALExecutionSucceeds(t *testing.T){
+	scenarios := Initialize()
+
+	//Let's work with last Scenario
+	scenario := scenarios[len(scenarios) -1]
+
+	scenario.Prepare(context.Background())
+
+	for _, m := range scenario.models {
+		args := []string{
+			"nonmem",
+			"run",
+			"--nmVersion",
+			os.Getenv("NMVERSION"),
+			"--nmqual=true",
+			"local",
+		}
+
+		output, err := m.Execute(scenario, args...)
+
+		nmd := NonMemTestingDetails{
+			t:         t,
+			OutputDir: filepath.Join(scenario.Workpath,m.identifier),
+			Model:     m,
+			Output:    output,
+		}
+
+		assert.Nil(t,err)
+		AssertNonMemCompleted(nmd)
+		AssertNonMemCreatedOutputFiles(nmd)
+		AssertScriptContainsAutologReference(nmd)
+	}
+}
+
+func AssertScriptContainsAutologReference(details NonMemTestingDetails){
+	scriptFile, _  := os.Open(filepath.Join(details.OutputDir,details.Model.identifier + ".sh"))
+	bytes, _ := ioutil.ReadAll(scriptFile)
+	scriptFile.Close()
+	scriptFileContent := string(bytes)
+
+	assert.Contains(details.t,scriptFileContent,"autolog.pl")
+}
