bd1ef9e2d6d480ad0bdede2469635edb2902f675
Darrell Breeden <darrellb@metrumrg.com>
2020-04-07 12:54:06 -0400

    feature: testing (by feature flag) of post execution. Includes sane path and verifies that values record files, they show failure, and the error status occurred in the execution output

--- postexecution_test.go
+++ postexecution_test.go
@@ -70,11 +70,12 @@ func TestPostExecutionSucceeds(t *testing.T){
 					//Does the file contain the expected Details:
 					//SCENARIO (Additional provided value)
 					file, _ := os.Open(filepath.Join("/tmp","working",v.identifier, m.identifier + ".out"))
-
-					scanner := bufio.NewScanner(file)
-					scanner.Split(bufio.ScanLines)
+					defer file.Close()
 
 					var lines []string
+
+					scanner := bufio.NewScanner(file)
+					//scanner.Split(bufio.ScanLines)
 
 					for scanner.Scan() {
 						lines = append(lines,scanner.Text())
@@ -91,6 +92,53 @@ func TestPostExecutionSucceeds(t *testing.T){
 
 			}
 		}
+
+		//Test a scenario for the first scenario where we force failure. Model is deleted (not found)
+		t.Run("verify_failure_results", func(t *testing.T){
+
+			var lines []string
+
+			scenario := Scenarios[0]
+			scenario.Prepare(context.Background())
+
+			arguments := []string{
+				"nonmem",
+				"--nm_version",
+				os.Getenv("NMVERSION"),
+				"run",
+				"local",
+				"--post_work_executable",
+				"/tmp/post.sh",
+				"--overwrite=false",
+				"--additional_post_work_envs=\"SCENARIO=" + scenario.identifier + "\"",
+			}
+
+			//Removing the model won't do anything. Execute with overwrite = false?
+			for _, v := range scenario.models {
+				os.Remove(filepath.Join(scenario.Workpath,v.identifier + ".out"))
+				output, err := v.Execute(scenario, arguments...)
+
+				//Does the file contain the expected Details:
+				//SCENARIO (Additional provided value)
+				file, _ := os.Open(filepath.Join("/tmp","working",scenario.identifier, v.identifier + ".out"))
+				defer file.Close()
+
+				scanner := bufio.NewScanner(file)
+				//scanner.Split(bufio.ScanLines)
+
+				for scanner.Scan() {
+					lines = append(lines,scanner.Text())
+				}
+
+
+
+				assert.NotNil(t,err)
+				assert.Error(t,err)
+
+				assert.True(t, doesOutputFileContainKeyWithValue(lines, "BABYLON_SUCCESSFUL", "false"))
+				assert.True(t, doesExecutionOutputContainErrorString(err.Error(), output))
+			}
+		})
 	}
 }
 
@@ -101,6 +149,20 @@ func doesOutputFileContainKeyWithValue(lines []string, key string, value string)
 		if strings.Contains(v,key+"=") {
 			components := strings.Split(v,"=")
 			return components[0] == key && components[1] == value
+		}
+	}
+
+	return false
+}
+
+func doesExecutionOutputContainErrorString(line string, output string) bool {
+
+	lines := strings.Split(output, "\n")
+
+	for _, v := range lines {
+		if strings.Contains(line, v){
+			//We have a match
+			return true
 		}
 	}
 
