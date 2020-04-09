99e67517bb62cffe3bfd349ff9e5969e48db2b0a
Darrell Breeden <darrellb@metrumrg.com>
2020-04-09 14:01:05 -0400

    fix: removing unused variable

--- bbi_sge_test.go
+++ bbi_sge_test.go
@@ -82,10 +82,6 @@ func TestBabylonCompletesParallelSGEExecution(t *testing.T){
 		"ctl_test",
 	})
 
-	whereami, _ := os.Getwd()
-
-
-
 	//Test shouldn't take longer than 5 min in total
 	//TODO use the context downstream in a runModel function
 	ctx, cancel := context.WithTimeout(context.Background(),5 * time.Minute)
