259aaa2ea5ab211c93cfa751f531dc281e12fe2a
Darrell Breeden <darrellb@metrumrg.com>
2021-09-17 11:02:32 -0400

    fix: noFilePresentError doesn't begin with a capital n when expressed

--- setup.go
+++ setup.go
@@ -26,7 +26,7 @@ var EXECUTION_DIR string
 const SUMMARY_TEST_DIR = "testdata/bbi_summary"
 const SUMMARY_GOLD_DIR = "aa_golden_files"
 const noSuchFileError = "no such file or directory"
-const noFilePresentError = "No file present at"
+const noFilePresentError = "no file present at"
 const wrongExtensionError = "Must provide path to .lst"
 
 type Scenario struct {
